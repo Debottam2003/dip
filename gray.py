052cce0f2c8c243c5aea3a23cf5fd31744b6aa5e
Debottam Kar <debottamkar2003@gmail.com>
2025-09-18 17:39:26 +0530

    rgb

---
+++ gray.py
@@ -0,0 +1,26 @@
+
+#* 4 bit gray scale image total 2^4 = 16 different possible gray shades(intensities).   
+#* (The blackest black to the whitest white.)
+
+#? Example
+
+shades_of_gray = [
+          "0000", #0
+          "0001", #1
+          "0010", #2
+          "0011", #3
+          "0100", #4
+          "0101", #5
+          "0110", #6
+          "0111", #7
+          "1000", #8
+          "1001", #9
+          "1010", #10
+          "1011", #11
+          "1100", #12
+          "1101", #13
+          "1110", #14
+          "1111"  #15
+          ]
+
+print(shades_of_gray)
